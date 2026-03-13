3691a78d1bebd7043681c310831bc619e0ff492e
tbaddade <thomas@redaxo.org>
2026-03-13 11:30:46 +0100

    Helper Class für aria

---
+++ lib/Util/Aria.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Yakamara\Roadie\Util;
+
+use Symfony\Component\Uid\Uuid;
+
+final class Aria
+{
+    private static string $lastId = '';
+
+    /**
+     * Generiert eine eindeutige ID für aria-Attribute und speichert sie für lastId().
+     * Die ID beginnt garantiert mit einem Buchstaben (a–p) und ist gültiges HTML.
+     */
+    public static function id(): string
+    {
+        $hex = str_replace('-', '', Uuid::v4()->toRfc4122());
+        return self::$lastId = chr(ord('a') + hexdec($hex[0])) . substr($hex, 1, 7);
+    }
+
+    /**
+     * Gibt die zuletzt generierte ID zurück, ohne eine neue zu erzeugen.
+     */
+    public static function lastId(): string
+    {
+        return self::$lastId;
+    }
+}
